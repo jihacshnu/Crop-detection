e587319af6370c01f3a5a76bd578b04dea19d9df
Jishnu Chowdhury <66846221+jihacshnu@users.noreply.github.com>
2024-03-07 00:13:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+import os
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from skimage import io
+from tensorflow.keras.preprocessing import image
+
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+
+# You can also use pretrained model from Keras
+    
+
+model =tf.keras.models.load_model('PlantDNet.h5',compile=False)
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, grayscale=False, target_size=(64, 64))
+    show_img = image.load_img(img_path, grayscale=False, target_size=(64, 64))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = np.array(x, 'float32')
+    x /= 255
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        print(preds[0])
+
+        # x = x.reshape([64, 64]);
+        disease_class = ['Apple Scab Leaf', 'Apple leaf', 'Apple rust leaf', 'Bell pepper leaf spot', 'Bell pepper leaf', 'Blueberry leaf', 'Cherry leaf', 'Corn Gray leaf spot', 'Corn leaf blight', 'Corn rust leaf', 'Peach leaf', 'Potato leaf early blight', 'Potato leaf late blight', 'Potato leaf', 'Raspberry leaf', 'Soyabean leaf', 'Soybean leaf', 'Squash Powdery mildew leaf', 'Strawberry leaf', 'Tomato Early blight leaf', 'Tomato Septoria leaf spot', 'Tomato leaf bacterial spot', 'Tomato leaf late blight', 'Tomato leaf mosaic virus', 'Tomato leaf yellow virus', 'Tomato leaf', 'Tomato mold leaf', 'Tomato two spotted spider mites leaf', 'grape leaf black rot', 'grape leaf']
+        ar = preds[0]
+        ind=np.argmax(ar)
+        print('Prediction:', disease_class[ind])
+        result=disease_class[ind]
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    # app.run(port=5002, debug=True)
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
+    app.run()
